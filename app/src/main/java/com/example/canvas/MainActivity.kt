7e1be2a5ee26433aa52d2f4916b75d3016c1d828
antonioolvera1995 <antonioolvera1995@gmail.com>
2021-09-06 17:30:57 +0200

    working in config

--- app/src/main/java/com/example/canvas/MainActivity.kt
+++ app/src/main/java/com/example/canvas/MainActivity.kt
@@ -5,28 +5,51 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.view.View.SYSTEM_UI_FLAG_FULLSCREEN
+import android.view.View.inflate
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
+import com.example.canvas.databinding.ActivityMainBinding
 import com.example.canvas.main_canvas.MainCanvas
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 
 class MainActivity : AppCompatActivity() {
 
 
     lateinit var mainCanvas: MainCanvas
-
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mainCanvas = MainCanvas(this)
         mainCanvas.systemUiVisibility = SYSTEM_UI_FLAG_FULLSCREEN
         mainCanvas.contentDescription = getString(R.string.canvasContentDescription)
-        setContentView(R.layout.activity_main)
+        val inflate = inflate(applicationContext, R.layout.activity_main, null)
+        inflate.findViewById<LinearLayout>(R.id.ly_canvas).addView(mainCanvas)
 
 
 
 
+        inflate.findViewById<Button>(R.id.bt_canvas).setOnClickListener {
 
+            GlobalScope.launch(Dispatchers.Main) {
+
+                var text = ""
+                withContext(Dispatchers.IO) {
+                    text = mainCanvas.controller.parseToJpeg()
+                }
+                inflate.findViewById<TextView>(R.id.tv_canvas).text = text.substring(text.length - 10, text.length)
+            }
+
+
+        }
+
+        setContentView(inflate)
     }
-
 
 
 }
